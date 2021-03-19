41d271471cf4162fefa80caede5eaa58128ca92d
firs-iln <firs_iln@mail.ru>
2021-03-20 01:15:10 +0300

    ggg

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from jinja2 import Template
 from os.path import join as path_to
 
@@ -10,6 +10,12 @@ def index():
     with open(path_to('front', 'index.html'), encoding='utf-8') as html:
         with open(path_to('front', 'styles.css'), encoding='utf-8') as css:
             return Template(html.read()).render({'style': css.read()})
+
+
+@app.route('/path/<path>')
+def send_path(path):
+    with open(path_to('front', f'path_{path}.geojson'), encoding='utf-8') as file:
+        return file.read()
 
 
 if __name__ == '__main__':
