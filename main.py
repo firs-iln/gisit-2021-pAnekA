d589e24033655a8cf1aedcd16c39eb12ec4c7af7
firs-iln <firs_iln@mail.ru>
2021-03-20 00:17:11 +0300

    все для бэка кроме бд

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from jinja2 import Template
+from os.path import join as path_to
 
-app = Flask
+app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    with open()
+    with open(path_to('front', 'index.html'), encoding='utf-8') as html:
+        with open(path_to('front', 'styles.css'), encoding='utf-8') as css:
+            return Template(html.read()).render({'style': css.read()})
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080)
