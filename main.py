a4da96a0c2e34de57093440cc1f414e6a9d1649d
firs-iln <firs_iln@mail.ru>
2021-03-20 21:13:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,50 @@
-from flask import Flask, request
+from flask import Flask
 from jinja2 import Template
 from os.path import join as path_to
+from pyproj import Proj, transform
+from pyproj import Transformer
+import json
 
 app = Flask(__name__)
+
+
+def convert_elem(elem: list):
+    elem = list(reversed(convert(elem[0], elem[1])))
+    return elem
+
+
+def parse(geojson):
+    geojson = geojson['features']
+    res = [{
+        "type": "Feature",
+        "properties": {
+            "info": "Path 9"
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[]
+            ]
+        }
+    }]
+    print(type(geojson), type(res))
+    for elem in geojson:
+        elem = elem['properties']
+        if elem == geojson[0]:
+            elem1 = list(map(float, elem['start'].split(', ')))
+            elem2 = list(map(float, elem['end'].split(', ')))
+            elem1, elem2 = convert_elem(elem1), convert_elem(elem2)
+            res[0]['geometry']['coordinates'][0].append(elem1)
+            res[0]['geometry']['coordinates'][0].append(elem2)
+        else:
+            elem = list(map(float, elem['end'].split(', ')))
+            elem = convert_elem(elem)
+            res[0]['geometry']['coordinates'][0].append(elem)
+    return json.dumps(res)
+
+
+def convert(long: float, lat: float):
+    x2, y2 = Transformer.from_crs('epsg:32652', 'epsg:4326').transform(long, lat)
+    return x2, y2
 
 
 @app.route('/')
@@ -13,10 +55,16 @@ def index():
 
 
 @app.route('/path/<path>')
-def send_path(path):
+def send_path(path: int):
     with open(path_to('front', f'path_{path}.geojson'), encoding='utf-8') as file:
-        return file.read()
+        file = file.read()
+        print(type(file))
+        file = json.loads(file)
+        file = parse(file)
+        print(file)
+        return file
 
 
 if __name__ == '__main__':
+    print(1, convert(508021.130256, 6816885.36582), convert(508471.998019, 6817029.18292))
     app.run(host='127.0.0.1', port=8080)
